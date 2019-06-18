53220dc09417d3c2e430d42b3aaf7fc44e076ebf
lexi-jayy <lexijayy94@gmail.com>
2019-06-18 15:27:43 +1200

    added images to home

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch');
  
  
     // Default task(s).
